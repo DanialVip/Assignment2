6607a0ab33069041fa860df749e1d328b16f6f6a
DanialVip <74869146+DanialVip@users.noreply.github.com>
2021-10-10 23:20:37 +0600

    Add files via upload

---
+++ ArticleFinder.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+class Finder():
+    def findArticle(self, crypto):
+        url = "https://www.google.com/search?q=site%3Acoinmarketcap.com+alexandria+" + crypto
+        req = requests.get(url)
+        soup = BeautifulSoup(req.content, 'html.parser')
+        listOfLinks = []
+        for link in soup.find_all('a'):
+            href = link.get('href')
+            if "https://coinmarketcap.com/" in href:
+                realLink = "https://www.google.com"+href
+                listOfLinks.append(realLink)
+        return listOfLinks
+
+
+
+
